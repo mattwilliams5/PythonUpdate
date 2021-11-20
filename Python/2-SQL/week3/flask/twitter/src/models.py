51fe04ffeee4b18ffcc20201b5de54167dbed4dd
Matt Williams <matt_williams_5@hotmail.com>
2021-11-20 12:41:04 -0500

    Updated to visualize psql

--- Python/2-SQL/week3/flask/twitter/src/models.py
+++ Python/2-SQL/week3/flask/twitter/src/models.py
@@ -11,7 +11,7 @@ db = SQLAlchemy()
 
 class User(db.Model):
     def __init__(self, password: str, username: int) -> None:
-        self.username = username
+        self.u = username
         self.password = password
 
     __tablename__ = 'users'
